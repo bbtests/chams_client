d61f35893b984f2225f0171883ca4beffaaf1919
Brotherbond <ehinmitankehinde@gmail.com>
2024-01-01 15:00:42 +0100

    ...

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,9 @@ export default async function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL(routes.dashboard.overview, req.url));
     }
     else if (req.nextUrl.pathname.startsWith('/dashboard') && !session?.email) {
-      return NextResponse.redirect(new URL(routes.auth.login, req.url));
+      const loginUrl = new URL(routes.auth.login, req.url);
+      loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname + req.nextUrl.search);
+      return NextResponse.redirect(loginUrl);
     }
     return NextResponse.next();
   } catch (error) {
